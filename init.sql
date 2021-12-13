f31a8adb3f4265a177824121728bf6c5df6bad15
Henrich Hegedus <henri.hegedus@gmail.com>
2021-12-13 16:19:26 +0000

    restructuring first commit

---
+++ init.sql
@@ -0,0 +1,31 @@
+create schema arbitrage;
+
+CREATE TABLE arbitrage.scrape(
+                     ID SERIAL PRIMARY KEY,
+                     bookie VARCHAR(20),
+                     bet_id INTEGER,
+                     event_date DATE,
+                     event_time TIME,
+                     sport VARCHAR(50),
+                     competition VARCHAR(100),
+                     team1 VARCHAR(200),
+                     team2 VARCHAR(200),
+                     odds1 FLOAT(24),
+                     oddsx FLOAT(24),
+                     odds2 FLOAT(24)
+);
+
+CREATE TABLE arbitrage.arbs(
+                     ID SERIAL PRIMARY KEY,
+                     sport VARCHAR(50),
+                     competition VARCHAR(100),
+                     team1 VARCHAR(200),
+                     team2 VARCHAR(200),
+                     odds1 FLOAT(24),
+                     oddsx FLOAT(24),
+                     odds2 FLOAT(24),
+                     margin FLOAT(24),
+                     bookie1 VARCHAR(50),
+                     bookiex VARCHAR(50),
+                     bookie2 VARCHAR(50)
+);
