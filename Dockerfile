5d435cd42645bfa4231782b8ba7d54fbc06baf59
Henrich Hegedus <henri.hegedus@gmail.com>
2021-12-26 14:56:14 +0000

    add cron job

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,13 @@ WORKDIR /code
 COPY requirements.txt /code/
 RUN pip install -r requirements.txt
 COPY . /code/
+
+RUN apt-get update
+RUN apt-get -qq -y install curl
+
+RUN apt-get install -y cron     # Install cron
+COPY scripts/cron /etc/chron.d/cron
+RUN chmod 0644 /etc/chron.d/cron #execution rights for chron job
+RUN crontab /etc/chron.d/cron    # apply chron job
+RUN touch /var/log/cron.log
+CMD cron && tail -f /var/log/cron.log
---
+++ scripts/clean_db.py
@@ -0,0 +1,19 @@
+import psycopg2
+import yaml
+stream = open("config/db_config.yaml", 'r')
+config = yaml.load(stream,Loader=yaml.FullLoader)
+connection = psycopg2.connect(user=config["user"],
+                                   password=config["password"],
+                                   host=config["host"],
+                                   port=config["port"],
+                                   database=config["database"])
+cursor = connection.cursor()
+
+query = """
+            delete from arbitrage.scrape where event_date < CURRENT_DATE; 
+            delete from arbitrage.arbs where date < CURRENT_DATE;
+        """
+
+cursor.execute(query)
+cursor.close()
+connection.close()
