8bb66809f1337c0a9c171bbe93fe56d6297302d8
Henrich Hegedus <henri.hegedus@gmail.com>
2021-12-26 14:56:22 +0000

    change db structure

--- init.sql
+++ init.sql
@@ -3,13 +3,12 @@ create schema arbitrage;
 CREATE TABLE arbitrage.scrape(
                      ID SERIAL PRIMARY KEY,
                      bookie VARCHAR(20),
-                     bet_id INTEGER,
+                     bet_id INTEGER UNIQUE,
                      event_date DATE,
                      event_time TIME,
                      sport VARCHAR(50),
                      competition VARCHAR(100),
                      team1 VARCHAR(200),
-                     team2 VARCHAR(200),
                      odds1 FLOAT(24),
                      oddsx FLOAT(24),
                      odds2 FLOAT(24)
@@ -18,14 +17,16 @@ CREATE TABLE arbitrage.scrape(
 CREATE TABLE arbitrage.arbs(
                      ID SERIAL PRIMARY KEY,
                      sport VARCHAR(50),
-                     competition VARCHAR(100),
-                     team1 VARCHAR(200),
-                     team2 VARCHAR(200),
+                     match VARCHAR(100),
+                     date DATE,
+                     time TIME,
                      odds1 FLOAT(24),
                      oddsx FLOAT(24),
                      odds2 FLOAT(24),
                      margin FLOAT(24),
                      bookie1 VARCHAR(50),
                      bookiex VARCHAR(50),
-                     bookie2 VARCHAR(50)
+                     bookie2 VARCHAR(50),
+                     CONSTRAINT date_match UNIQUE (date,match)
+
 );
